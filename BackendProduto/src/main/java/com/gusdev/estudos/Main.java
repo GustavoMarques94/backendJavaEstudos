0bc26d14505ed6402ea53d0b825e500380ff2ea1
GustavoMarques94 <marques.lameirao@gmail.com>
2024-09-17 01:11:50 -0300

    Teste

--- BackendProduto/src/main/java/com/gusdev/estudos/Main.java
+++ BackendProduto/src/main/java/com/gusdev/estudos/Main.java
@@ -2,6 +2,6 @@ package com.gusdev.estudos;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Olá!");
     }
 }
